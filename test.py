9ec91b8cf835046a9f3eaae7a08686f7523e4d3f
Sunch Technology Company <106715747+ZackerySunch@users.noreply.github.com>
2026-05-04 11:41:25 +0800

    create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+c = input()
+d = input()
+print("|{:>5s}{:>5s}|".format(a,b))
+print("|{:>5s}{:>5s}|".format(c,d))
+print("|{:<5s}{:<5s}|".format(a,b))
+print("|{:<5s}{:<5s}|".format(c,d))
+
+
+
+
+print("{:>.2f}{}".format())
